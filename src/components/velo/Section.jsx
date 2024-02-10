118f2279f19e21b25acb4c5314374d11581478cf
DIILEESHA <dileeeshanawarathna18@gmail.com>
2024-02-10 18:20:41 +0530

    fix text overlapping

--- src/components/velo/Section.jsx
+++ src/components/velo/Section.jsx
@@ -3,15 +3,15 @@
 
 import React from "react";
 import Velo from "./Velo";
-import './velo.css'
+import "./velo.css";
 
 function ParallaxSection() {
   return (
     <div className="sd">
       <Velo style={{ fontSize: "10px" }} baseVelocity={0.2}>
-        <h1 style={{fontSize:"30px"}}>
-
-      Available from JANUARY 2023, always looking to collaborate with creators and creative teams. Let’s talk!
+        <h1 style={{ fontSize: "30px" }}>
+          Available from JANUARY 2023, always looking to collaborate with
+          creators and creative teams. Let’s talk!
         </h1>
       </Velo>
       {/* <Velo style={{ fontSize: "50px" }} baseVelocity={2}>
