b26b9b2618173e70efc5930423174082e1952d05
DIILEESHA <dileeeshanawarathna18@gmail.com>
2024-03-13 22:31:38 +0530

    config firebase database permissions and fix some bugs

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,9 @@ export default function Layout() {
   return (
     <div>
       <Card />
+      <div style={{ position: "relative", zIndex: "1" }} className="">
         <Nav />
+      </div>
 
       <div className="rounder_img">
         <img loading="lazy" src={back} alt="" />
